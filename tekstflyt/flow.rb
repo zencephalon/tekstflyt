772e15ce9f4dbfff014c02cccb99929b14d049a3
Matthew Bunday <zenceph@gmail.com>
2013-08-03 00:34:33 -0400

    Finished flow model

--- tekstflyt/flow.rb
+++ tekstflyt/flow.rb
@@ -9,12 +9,20 @@ Flow = Struct.new :_id, :text, :title, :score do
 end
 
 class FlowManager
-    RUBY_TO_MONGO = {_id: :_id, text: :tx, title: :tl, score: :s, number: :n}.freeze
+    RUBY_TO_MONGO = {_id: :_id, text: :tx, writer: :w, title: :tl, score: :s, number: :n}.freeze
     MONGO_TO_RUBY = RUBY_TO_MONGO.invert.freeze
 
     def initialize(tekstflyt)
         @tekstflyt = tekstflyt
         @flow_db = @tekstflyt.db.collection('flows')
+    end
+
+    def mongo_to_ruby(mongo_obj)
+        flow = Flow.new
+        mongo_obj.each do |key, val| 
+            flow[MONGO_TO_RUBY[key.to_sym]] = val if val
+        end
+        return flow
     end
 
     def create(writer, text, score)
@@ -24,6 +32,7 @@ class FlowManager
         flow.text = text
         flow.title = Time.now.strftime("%D %H:%M")
         flow.score = score
+        flow.writer = writer._id
 
         mongo_obj = flow.to_mongo
         @flow_db.insert(mongo_obj)
@@ -31,5 +40,12 @@ class FlowManager
         return flow
     end
 
+    def get(writer, number)
+        flow = @flow_db.find_one({:w => writer._id, :n => n.to_i})
+        return flow ? mongo_to_ruby(flow) : nil
+    end
 
+    def get_all_by_writer(writer)
+        @flow_db.find({:w => writer._id}).to_a
+    end
 end

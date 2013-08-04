333582ce31fd052f787625d502c01f522114d241
Daria Jung <djj2115cu@gmail.com>
2013-08-04 00:15:34 -0400

    prevent users from typing in anything other than integers + error messages

---
+++ public/js/mode.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $(".form-wordcount").on("submit", function(event) {
+    	var wordcount = $('#wordcount').val();
+		if (!(isInt(wordcount) && wordcount.length > 0))  {
+			event.preventDefault();
+			$('.error-wordcount').css('display', 'block');
+		} 
+	});
+
+	$(".form-timer").on("submit", function(event) {
+		var timer = $('#timer').val();
+		if (!(isInt(timer) && timer.length > 0)) {
+			event.preventDefault();
+			$('.error-timer').css('display', 'block');
+		}
+	});
+});
+
+function isInt(n) {
+   return n % 1 === 0;
+}

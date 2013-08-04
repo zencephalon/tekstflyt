9aa04e9623af8431c6683ff4e58e19d72b441b8f
Daria Jung <djj2115cu@gmail.com>
2013-08-04 02:45:37 -0400

    prevent users from signing up without filling in all of form

---
+++ public/js/signup.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $("form").on("submit", function(event) {
+    	var username = $('#username').val();
+    	var password = $('#password').val();
+    	var pw_confirm = $('#password-confirm').val();
+
+		if (username.length <= 0 || password.length <= 0 || pw_confirm.length <= 0)  {
+			event.preventDefault();
+			$('.error-signup').css('display', 'block');
+		} 
+	});
+
+	$('.clear').on('click', function() {
+		$('.error-signup').css('display', 'none');
+	});
+});

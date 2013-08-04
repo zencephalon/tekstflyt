a4a212235784670d3cd191660c9919473aea6cec
Daria Jung <djj2115cu@gmail.com>
2013-08-04 14:34:31 -0400

    signup password check

--- public/js/signup.js
+++ public/js/signup.js
@@ -7,11 +7,22 @@ $( document ).ready(function() {
 
 		if (username.length <= 0 || password.length <= 0 || pw_confirm.length <= 0)  {
 			event.preventDefault();
-			$('.error-signup').css('display', 'block');
+			$('.error-pw-match').css('display', 'none');
+			$('.error-signup').css('display', 'block');	
 		} 
+
+		if(password !== pw_confirm) {
+			event.preventDefault();
+			$('.error-pw-match').css('display', 'block');
+
+			if (username.length > 0) {
+				$('.error-signup').css('display', 'none');
+			}
+		}
 	});
 
 	$('.clear').on('click', function() {
 		$('.error-signup').css('display', 'none');
+		$('.error-pw-match').css('display', 'none');
 	});
 });

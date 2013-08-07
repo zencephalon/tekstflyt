76f38e5401c483fc9a130a78caf924b042581220
Matthew Bunday <zenceph@gmail.com>
2013-08-07 19:56:11 -0400

    Move into function

--- public/js/tekstflyt.js
+++ public/js/tekstflyt.js
@@ -119,11 +119,7 @@ function saveFlowToServer() {
 function freezeFlowAndReset(text) {
     $('.tekstflyt').before("<div class='text-content'><p>" + text + "\n" + "</p></div>");
     $('#playingfield').val("");
-    $('html, body').animate({ 
-        scrollTop: $(document).height()-$(window).height()}, 
-        1400, 
-        "linear"
-        );
+    scroll();
 }
 
 function displayEncouragement(score) {
@@ -183,6 +179,14 @@ function enableSaveButton() {
     });
 }
 
+function scroll() {
+    $('html, body').animate({ 
+        scrollTop: $(document).height()-$(window).height()}, 
+        1400, 
+        "linear"
+        );
+}
+
 // ============================== Helpers =====================================
 function getTime() {
     return (new Date).getTime();
@@ -193,10 +197,6 @@ function getElapsedSeconds(time) {
 }
 
 document.getElementById("playingfield").oninput = updateFlowState;
-
-function scroll() {
-    $("html, body").animate({ scrollTop: $('#playingfield').offset().top }, "slow");
-}
 
 // scores should be higher the longer the text is
 // scores should be higher the longer the time is
